5a812ffa442aa2f92093017fed05f98b2c058ec5
Vladimir Novikov <novikovvlal@gmail.com>
2022-12-22 23:21:11 +0300

    38. Movement Offset Yaw

--- Source/Shooter/Character/Private/ShooterCharacterAnimInstance.cpp
+++ Source/Shooter/Character/Private/ShooterCharacterAnimInstance.cpp
@@ -4,6 +4,7 @@
 #include "Shooter/Character/Public//ShooterCharacterAnimInstance.h"
 
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "Shooter/Character/Public/ShooterCharacter.h"
 
 void UShooterCharacterAnimInstance::UpdateAnimationProperties(float DeltaTime)
@@ -16,7 +17,7 @@ void UShooterCharacterAnimInstance::UpdateAnimationProperties(float DeltaTime)
 	if (ShooterCharacter)
 	{
 		// Get the lateral speed of the character from velocity
-		FVector Velocity { ShooterCharacter->GetVelocity() };
+		FVector Velocity{ShooterCharacter->GetVelocity()};
 		Velocity.Z = 0;
 		Speed = Velocity.Size();
 
@@ -25,6 +26,19 @@ void UShooterCharacterAnimInstance::UpdateAnimationProperties(float DeltaTime)
 
 		// Is the character accelerating?
 		bIsAccelerating = ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f;
+
+		FRotator AimRotation = ShooterCharacter->GetBaseAimRotation();
+		// FString RotationMessage = FString::Printf(TEXT("Base Aim Rotation: %f"), AimRotation.Yaw);
+
+		FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(ShooterCharacter->GetVelocity());
+		// FString MovementRotationMessage = FString::Printf(TEXT("Movement Rotation: %f"), MovementRotation.Yaw);
+
+		MovementOffsetYaw = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, AimRotation).Yaw;
+		
+		// if (GEngine)
+		// {
+			// GEngine->AddOnScreenDebugMessage(1, 0.f, FColor::White, RotationMessage);
+		// }
 	}
 }
 
