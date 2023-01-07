725be6208575ad7c7e85c2f7abc5fbb6b57e09f6
Vladimir Novikov <novikovvlal@gmail.com>
2023-01-07 13:44:48 +0300

    101. The Weapon Type

--- Source/Shooter/Public/Items/Weapons/Weapon.h
+++ Source/Shooter/Public/Items/Weapons/Weapon.h
@@ -6,6 +6,14 @@
 #include "Items/Item.h"
 #include "Weapon.generated.h"
 
+UENUM(BlueprintType)
+enum class EWeaponType : uint8
+{
+	EWT_SubmachineGun UMETA(DisplayName = "SubmachineGun"),
+	EWT_AssaultRifle UMETA(DisplayName = "AssaultRifle"),
+	EWT_MAX UMETA(DisplayName = "DefaultMAX")
+};
+
 /**
  * 
  */
@@ -26,10 +34,15 @@ private:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
 	int32 Ammo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	EWeaponType WeaponType;
 public:
 	void ThrowWeapon();
 
 	FORCEINLINE int32 GetAmmo() const { return Ammo; }
 
 	void DecrementAmmo();
+
+	FORCEINLINE EWeaponType GetWeaponType() const { return WeaponType; }
 };
